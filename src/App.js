c3ab3a6e1a02176b82a558d33e6eeb06c4627aa1
Lise <lise692@gmail.com>
2022-11-20 14:43:41 +0100

    setting up first class

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Nouvelle application React.js | CodeCademy
         </a>
       </header>
     </div>
