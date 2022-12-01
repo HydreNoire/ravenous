c55af4aa29dfa0b6795e4b17af47ba8752726bda
Lise <lise692@gmail.com>
2022-12-01 14:05:55 +0100

    add CSS on each business

--- src/components/Business/Business.js
+++ src/components/Business/Business.js
@@ -2,8 +2,9 @@ import React from 'react';
 import './Business.css';
 
 class Business extends React.Component {
-    render() {
-        return (<div className="Business">
+  render() {
+    return (
+      <div className="Business">
         <div className="image-container">
           <img src={this.props.business.imageSrc} alt=''/>
         </div>
@@ -15,13 +16,14 @@ class Business extends React.Component {
             <p>{`${this.props.business.state} ${this.props.business.zipCode}`}</p>
           </div>
           <div className="Business-reviews">
-            <h3>{this.props.business.category}</h3>
-            <h3 className="rating">{this.props.business.rating}</h3>
-            <p>{this.props.business.reviewCount} reviews</p>
+            <h3>{this.props.business.category.toUpperCase()}</h3>
+            <h3 className="rating">{`${this.props.business.rating} stars`}</h3>
+            <p>{`${this.props.business.reviewCount} reviews`}</p>
           </div>
         </div>
-      </div>)
-    }
+      </div>
+    );
+  }
 }
 
 export default Business;
